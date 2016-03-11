bfd48df2bafc9ff5fc78db1fe59317fbaef9321c
Daragh Egan <degan@evestment.com>
2016-03-11 14:13:59 -0500

    Added Color object

---
+++ color.js
@@ -0,0 +1,353 @@
+/**
+ * Color object
+ * RGB Color with three 8-bit channels
+ * Each instance expresses color in three different ways:
+ *  - Hexidecimal: A 6-character string
+ *  - FlatInteger: A 24-bit integer encoding three bytes
+ *  - RGB: r, g, and b are three separate properties, 1 byte each
+ *  
+ *  valueOf() returns the FlatInteger representation.
+ *  toString() returns the hexidecimal representation.
+ *  toString('rgb') returns the rgb representation as a string.
+ *  
+ *  This allows shortcuts like document.body.style.color = new Color('ff7700');
+ */
+
+var Color = (function(){
+	var BYTE = 0xff;
+	var BYTE3 = 0xffffff;
+	
+	/*** Constructor ***/
+	function Color() {
+		// Initialize default values
+		this.hex='000000';
+		this.int=0;
+		this.r=0;
+		this.g=0;
+		this.b=0;
+		
+		if (arguments.length == 1) {
+			var c = arguments[0];
+			
+			// #hex
+			if (vld.hex(c)) {
+				// Parse hex
+				this.hex = parse.hex(c);
+				// Convert to int
+				this.int = cnv.hex_int(this.hex);
+				// Convert to rgb
+				var rgb = cnv.int_rgb(this.int);
+				this.r = rgb.r;
+				this.g = rgb.g;
+				this.b = rgb.b;
+			}
+			
+			// Flat integer
+			else if (vld.int(c)) {
+				// Parse int
+				this.int = parse.int(c);
+				// Convert to hex
+				this.hex = cnv.int_hex(this.int);
+				// Convert to rgb
+				var rgb = cnv.int_rgb(this.int);
+				this.r = rgb.r;
+				this.g = rgb.g;
+				this.b = rgb.b;
+			}
+			
+			else if (typeof(c) == 'object') {
+				if ( 
+					(vld.byte(c.r)) && 
+					(vld.byte(c.g)) && 
+					(vld.byte(c.b)) 
+					) {
+					var r = c.r;
+					var g = c.g;
+					var b = c.b;
+					return Color.call(this, r,g,b);
+					
+				}
+			}
+		}
+		else if (arguments.length == 3) {
+			var r = arguments[0];
+			var g = arguments[1];
+			var b = arguments[2];
+			
+			// RGB
+			if (vld.rgb(r,g,b)) {
+				// Parse rgb
+				var rgb = parse.rgb(r,g,b);
+				this.r = rgb.r;
+				this.g = rgb.g;
+				this.b = rgb.b;
+				// Convert to int
+				this.int = cnv.rgb_int(r,g,b);
+				// Convert to hex
+				this.hex = cnv.int_hex(this.int);
+			}
+			
+		}
+		
+		// Attach member functions
+		this.toString = function() 	{return util.tostr(this);}
+		this.valueOf = function() 	{return util.val(this);}
+        this.inv = function(c)      {return util.not(this);}
+		this.add = function(c) 		{return util.add(this, c);}
+		this.avg = function(c) 		{return util.avg(this, c);}
+		this.sub = function(c) 		{return util.sub(this, c);}
+		this.not = function() 		{return util.not(this);}
+		this.and = function(c) 		{return util.and(this, c);}
+		this.or = function(c) 		{return util.or(this, c);}
+		this.xor = function(c) 		{return util.xor(this, c);}
+        this.rgb = function()       {return [this.r, this.g, this.b];}
+	}
+	
+	/*** Utilities ***/
+	var util = {
+		tostr: function toString(c, fmt) {
+			if (fmt && fmt == 'rgb') {
+				return 'rgb('+c.r+','+c.g+','+c.b+')';
+			}
+			else {
+				return '#' + c.hex;
+			}
+		},
+		
+		val: function valueOf(c) {
+			return c.int;
+		},
+        
+		add: function add(x,y) {
+			return new Color(x.int + y.int);
+		},
+		
+		avg: function average(x,y) {
+			var rgb = parse.rgb(
+				(x.r + y.r)/2,
+				(x.g + y.g)/2,
+				(x.b + y.b)/2
+			);
+			return new Color(rgb);
+		},
+		
+		sub: function subtract(x,y) {
+			return new Color(x.int - y.int);
+		},
+		
+		not: function bitnot(x){
+			return new Color((BYTE3-x.int));
+		},
+		
+		and: function bitand(x,y){
+			return new Color(x.int & y.int);
+		},
+		
+		or: function bitor(x,y){
+			return new Color(x.int | y.int);
+		},
+		
+		xor: function bitxor(x,y){
+			return new Color(x.int ^ y.int);
+		},
+        
+		random: function random() {
+			return new Color(Math.floor(Math.random()*BYTE3));
+		}
+	};
+	
+	
+	/*** Validation ***/
+	var vld = {
+			
+		hex: function isvalidHex(str) {
+			return (typeof(str)=='string') && /^\s*#?[a-fA-F0-9]{1,6}\s*;?$/.test(str);
+		},
+		
+		int: function isvalidFlatInt(n) {
+			return (typeof(n)=='number') && (0<=n) && (n<=BYTE3);
+		},
+		
+		rgb: function isvalidRGB() {
+			var retval = true;
+			for (var i in arguments) {
+				retval &= vld.byte(arguments[i]);
+			}
+			return retval;
+		},
+		
+		byte: function isvalidByte(n) {
+			return (typeof(n)=='number') && (0<=n) && (n<=BYTE);
+		},
+		
+		color: function isvalidColor(c) {
+			return (
+				vld.hex(c.hex) &&
+				vld.int(c.int) &&
+				vld.byte(c.r) &&
+				vld.byte(c.g) &&
+				vld.byte(c.b)
+			);
+		}
+	};
+	
+	/*** Parsing ***/
+	var parse = {
+		hex: function parseHex(x) {
+            // Remove whitspace and hash sign
+            x = x.replace(/[\s#;]*/g,'');
+            // Handle 3-digit shorthand
+            if (x.length==3) {
+                var r = x.charAt(0);
+                var g = x.charAt(1);
+                var b = x.charAt(2);
+                x = r+r + g+g + b+b;
+            }
+            // Left pad with zeros
+            while (x.length < 6) {
+                x = '0'+x;
+            }
+            return x;
+		},
+		
+		int: function parseFlatInt(n) {
+            // Round to nearest valid value
+            if (n < 0)
+                return 0;
+            else if (n > BYTE3) {
+                return BYTE3;
+            }
+            else {
+                return n;
+            }
+		},
+		
+		rgb: function parseRGB(r,g,b) {
+			return { r: parse.byte(r), 
+					g: parse.byte(g), 
+					b: parse.byte(b)
+			};
+		},
+		
+		byte: function parseByte(n) {
+			var i = parseInt(n);
+			if (i && i>BYTE)
+				i=BYTE;
+			return i || 0;
+		}
+	}
+	
+	/*** Conversion ***/
+	var cnv = {
+		hex_int: function hexToInt(x){
+			var hex = parse.hex(x);
+			return parseInt(hex, 16);
+		},
+		
+		hex_rgb: function hexToRgb(x){
+			var int = cnv.hex_int(x);
+			return cnv.int_rgb(int);
+		},
+		
+		int_hex: function intToHex(n){
+			var int = parse.int(n);
+			return parse.hex(int.toString(16));
+		},
+		
+		int_rgb: function intToRgb(n){
+			var int = parse.int(n);
+			return {
+				r: (int & 0xff0000) >> 16,
+				g: (int & 0x00ff00) >> 8,
+				b:  int & 0x0000ff
+			}
+		},
+		
+		rgb_hex: function rgbToHex(r,g,b){
+			var int = cnv.rgb_int(r,g,b);
+			return cnv.int_hex(int);
+		},
+		
+		rgb_int: function rgbToInt(r,g,b){
+			return (parse.byte(r) << 16) + 
+					(parse.byte(g) << 8) + 
+					parse.byte(b);
+		}
+	};
+
+	/*** Static Properties ***/
+	Color.util = util;
+	
+    Color.inv = util.not;
+	Color.add = util.add;
+	Color.avg = util.avg;
+	Color.sub = util.sub;
+	Color.and = util.and;
+	Color.or = util.or;
+	Color.xor = util.xor;
+	Color.random = util.random;
+	
+	Color.WHITE = 	new Color(0xffffff);
+	Color.RED = 	new Color(0xff0000);
+	Color.GREEN = 	new Color(0x00ff00);
+	Color.BLUE = 	new Color(0x0000ff);
+	Color.YELLOW = 	new Color(0xffff00);
+	Color.CYAN = 	new Color(0x00ffff);
+	Color.MAGENTA = new Color(0xff00ff);
+	Color.BLACK = 	new Color(0x0);
+	
+	/** Return constructor **/
+	return Color;
+})();
+
+
+var rgb = function(r,g,b) {
+    return new Color(r,g,b);
+};
+
+function getHSL(color) {
+    var hsl = {
+        h: 0,
+        s: 0,
+        l: 0
+    };
+    var r = color.r;
+    var g = color.g;
+    var b = color.b;
+    
+    var min = 255;
+    var max = 0;
+    var arr = [r,g,b];
+    for (var i in arr) {
+        if (arr[i] > max) max = arr[i];
+        if (arr[i] < min) min = arr[i];
+    }
+    
+    var chroma = parseFloat(max-min);
+    
+    var H = 0;
+    
+    if ( r>g && r>b ) {
+        H = ((g-b)/chroma) % 6;
+    }
+    else if (g>r && g>b) {
+        H = ((b-r)/chroma) + 2;
+    }
+    else if (b>r && b>g) {
+        H = ((r-g)/chroma) + 4;
+    }
+    
+    hsl.h = 60*H;
+    hsl.l = (max + min) / 2;
+    
+    if (hsl.l <= 127.5) {
+        hsl.s = chroma/(2*hsl.l);
+    }
+    else {
+        hsl.s = chroma/(2*255 - 2*hsl.l);
+    }
+    
+    return hsl;
+}
+
+
