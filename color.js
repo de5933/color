0a8cf9d4e47de43da37c7be60d2180122bc2673e
Daragh Egan <degan@evestment.com>
2016-04-20 09:36:15 -0400

    Cleanup and improvements
    Added unit tests.
    Made hex and int into calculated properties.
    Throws exceptions instead of writing to console.error.
    Made add() and sub() use rgb components instead of int value.
    Formatting to use only spaces (no tabs)

--- color.js
+++ color.js
@@ -20,202 +20,202 @@
  */
 
 var Color = (function(){
-	var BYTE = 0xff;
-	var BYTE3 = 0xffffff;
-	
-	/*** Constructor ***/
-	function Color() {
-		// Initialize default values
-		this.hex='000000';
-		this.int=0;
-		this.r=0;
-		this.g=0;
-		this.b=0;
+    var BYTE = 0xff;
+    var BYTE3 = 0xffffff;
+    
+    /*** Constructor ***/
+    function Color() {
+        // Initialize default values
+        this.r=0;
+        this.g=0;
+        this.b=0;
         this.a=1;
-		
+        
         var success = false;
         
-		if (arguments.length == 1) {
-			var c = arguments[0];
-			
-			// #hex
-			if (vld.hex(c)) {
-				// Parse hex
-				this.hex = parse.hex(c);
-				// Convert to int
-				this.int = cnv.hex_int(this.hex);
-				// Convert to rgb
-				var rgb = cnv.int_rgb(this.int);
-				this.r = rgb.r;
-				this.g = rgb.g;
-				this.b = rgb.b;
+        // If no arguments, set to default color
+        if (arguments.length == 0) {
+            this.r = 0;
+            this.g = 0;
+            this.b = 0;
+            this.a = 1;
+            
+            success = true;
+        }
+        else if (arguments.length == 1) {
+            var c = arguments[0];
+            
+            // #hex
+            if (vld.hex(c)) {
+                // Convert to rgb
+                var rgb = cnv.hex_rgb(c);
+                this.r = rgb.r;
+                this.g = rgb.g;
+                this.b = rgb.b;
                 
                 success = true;
-			}
-			
-			// Flat integer
-			else if (vld.int(c)) {
-				// Parse int
-				this.int = parse.int(c);
-				// Convert to hex
-				this.hex = cnv.int_hex(this.int);
-				// Convert to rgb
-				var rgb = cnv.int_rgb(this.int);
-				this.r = rgb.r;
-				this.g = rgb.g;
-				this.b = rgb.b;
+            }
+            
+            // Flat integer
+            else if (vld.int(c)) {
+                // Convert to rgb
+                var rgb = cnv.int_rgb(c);
+                this.r = rgb.r;
+                this.g = rgb.g;
+                this.b = rgb.b;
                 
                 success = true;
-			}
-			
-			else if (typeof(c) == 'object') {
-				if ( 
-					(vld.byte(c.r)) && 
-					(vld.byte(c.g)) && 
-					(vld.byte(c.b)) 
-					) {
-					var r = c.r;
-					var g = c.g;
-					var b = c.b;
-                    
-					return Color.call(this, r,g,b);
-				}
-			}
+            }
             
-            
-		}
-		else if (arguments.length == 3 || arguments.length == 4) {
-			var r = arguments[0];
-			var g = arguments[1];
-			var b = arguments[2];
-			var a = arguments[3];
-            
-			// RGB
-			if (vld.rgb(r,g,b)) {
-				// Parse rgb
-				var rgba = parse.rgba(r,g,b,a);
-				this.r = rgba.r;
-				this.g = rgba.g;
-				this.b = rgba.b;
-                this.a = rgba.a;
-				// Convert to int
-				this.int = cnv.rgb_int(r,g,b);
-				// Convert to hex
-				this.hex = cnv.int_hex(this.int);
+            // RGB(A) object
+            else if (typeof(c) == 'object') {
+                this.r = parse.byte(c.r);
+                this.g = parse.byte(c.g);
+                this.b = parse.byte(c.b);
+                this.a = parse.unit(c.a);
                 
                 success = true;
-			}
-			
-		}
+            }
+            
+            
+        }
+        // R,G,B(,A) arguments
+        else if (arguments.length == 3 || arguments.length == 4) {
+            this.r = parse.byte(arguments[0]);
+            this.g = parse.byte(arguments[1]);
+            this.b = parse.byte(arguments[2]);
+            this.a = parse.unit(arguments[3]);
+            
+            success = true;
+        }
         
         if (!success) 
-            err('Constructor arguments not recognized: ' + JSON.stringify(arguments));
-		
-		// Attach member functions
-		this.toString = function() 	{return util.tostr(this);}
-		this.valueOf = function() 	{return util.val(this);}
+            throw ['InvalidArgumentsException' , 
+                ' Constructor arguments not recognized: ' + JSON.stringify(arguments)];
+        
+        // Computed values 
+        this.hex = function(){
+            return cnv.rgb_hex(this.r, this.g, this.b);
+        };
+        
+        this.int = function(){
+            return cnv.rgb_int(this.r, this.g, this.b);
+        };
+        
+        // Attach member functions
+        this.toString = function()  {return util.tostr(this);}
+        this.valueOf = function()   {return util.val(this);}
         this.inv = function(c)      {return util.not(this);}
-		this.add = function(c) 		{return util.add(this, c);}
-		this.avg = function(c) 		{return util.avg(this, c);}
-		this.sub = function(c) 		{return util.sub(this, c);}
-		this.not = function() 		{return util.not(this);}
-		this.and = function(c) 		{return util.and(this, c);}
-		this.or = function(c) 		{return util.or(this, c);}
-		this.xor = function(c) 		{return util.xor(this, c);}
+        this.add = function(c)      {return util.add(this, c);}
+        this.avg = function(c)      {return util.avg(this, c);}
+        this.sub = function(c)      {return util.sub(this, c);}
+        this.not = function()       {return util.not(this);}
+        this.and = function(c)      {return util.and(this, c);}
+        this.or = function(c)       {return util.or(this, c);}
+        this.xor = function(c)      {return util.xor(this, c);}
         this.rgb = function()       {return [this.r, this.g, this.b];}
         this.rgba = function()       {return [this.r, this.g, this.b, this.r];}
-	}
-	
-	/*** Utilities ***/
-	var util = {
-		tostr: function toString(c, fmt) {
-			if ( c.a<1 || (fmt && fmt == 'rgba') ) {
-				return 'rgba('+c.r+','+c.g+','+c.b+','+c.a+')';
-			}
-			else {
-				return '#' + c.hex;
-			}
-		},
-		
-		val: function valueOf(c) {
-			return c.int;
-		},
+    }
+    
+    /*** Utilities ***/
+    var util = {
+        tostr: function toString(c, fmt) {
+            if ( c.a<1 || (fmt && fmt == 'rgba') ) {
+                return 'rgba('+c.r+','+c.g+','+c.b+','+c.a+')';
+            }
+            else {
+                return '#' + c.hex();
+            }
+        },
         
-		add: function add(x,y) {
-			return new Color(x.int + y.int);
-		},
-		
-		avg: function average(x,y) {
-			var rgb = parse.rgb(
-				(x.r + y.r)/2,
-				(x.g + y.g)/2,
-				(x.b + y.b)/2
-			);
-			return new Color(rgb);
-		},
-		
-		sub: function subtract(x,y) {
-			return new Color(x.int - y.int);
-		},
-		
-		not: function bitnot(x){
-			return new Color((BYTE3-x.int));
-		},
-		
-		and: function bitand(x,y){
-			return new Color(x.int & y.int);
-		},
-		
-		or: function bitor(x,y){
-			return new Color(x.int | y.int);
-		},
-		
-		xor: function bitxor(x,y){
-			return new Color(x.int ^ y.int);
-		},
+        val: function valueOf(c) {
+            return c.int();
+        },
         
-		random: function random() {
-			return new Color(Math.floor(Math.random()*BYTE3));
-		}
-	};
-		
-	/*** Validation ***/
-	var vld = {
-			
-		hex: function isvalidHex(str) {
-			return (typeof(str)=='string') && /^\s*#?[a-fA-F0-9]{1,6}\s*;?$/.test(str);
-		},
-		
-		int: function isvalidFlatInt(n) {
-			return (typeof(n)=='number') && (0<=n) && (n<=BYTE3);
-		},
-		
-		rgb: function isvalidRGB() {
-			var retval = true;
-			for (var i in arguments) {
-				retval &= vld.byte(arguments[i]);
-			}
-			return retval;
-		},
-		
-		byte: function isvalidByte(n) {
-			return (typeof(n)=='number') && (0<=n) && (n<=BYTE);
-		},
-		
-		color: function isvalidColor(c) {
-			return (
-				vld.hex(c.hex) &&
-				vld.int(c.int) &&
-				vld.byte(c.r) &&
-				vld.byte(c.g) &&
-				vld.byte(c.b)
-			);
-		}
-	};
-	
-	/*** Parsing ***/
-	var parse = {
-		hex: function parseHex(x) {
+        add: function add(x,y) {
+            return new Color(
+                x.r + y.r,
+                x.g + y.g,
+                x.b + y.b,
+                x.a + y.a
+            );
+        },
+        
+        avg: function average(x,y) {
+            var rgb = parse.rgb(
+                (x.r + y.r)/2,
+                (x.g + y.g)/2,
+                (x.b + y.b)/2
+            );
+            return new Color(rgb);
+        },
+        
+        sub: function subtract(x,y) {
+            return new Color(
+                x.r - y.r,
+                x.g - y.g,
+                x.b - y.b
+            );
+        },
+        
+        not: function bitnot(x){
+            return new Color( clamp(BYTE3-x.int(), 0, BYTE3) );
+        },
+        
+        and: function bitand(x,y){
+            return new Color( clamp(x.int() & y.int(), 0, BYTE3) );
+        },
+        
+        or: function bitor(x,y){
+            return new Color( clamp(x.int() | y.int(), 0, BYTE3) );
+        },
+        
+        xor: function bitxor(x,y){
+            return new Color( clamp(x.int() ^ y.int(), 0, BYTE3) );
+        },
+        
+        random: function random() {
+            return new Color(Math.floor(Math.random()*BYTE3));
+        }
+    };
+        
+    /*** Validation ***/
+    var vld = {
+            
+        hex: function isvalidHex(str) {
+            return (typeof(str)=='string') && /^\s*#?[a-fA-F0-9]{1,6}\s*;?$/.test(str);
+        },
+        
+        int: function isvalidFlatInt(n) {
+            return (typeof(n)=='number') && (0<=n) && (n<=BYTE3);
+        },
+        
+        rgb: function isvalidRGB() {
+            var retval = true;
+            for (var i in arguments) {
+                retval &= vld.byte(arguments[i]);
+            }
+            return retval;
+        },
+        
+        byte: function isvalidByte(n) {
+            return (typeof(n)=='number') && (0<=n) && (n<=BYTE);
+        },
+        
+        color: function isvalidColor(c) {
+            return (
+                vld.hex(c.hex) &&
+                vld.int(c.int()) &&
+                vld.byte(c.r) &&
+                vld.byte(c.g) &&
+                vld.byte(c.b)
+            );
+        }
+    };
+    
+    /*** Parsing ***/
+    var parse = {
+        hex: function parseHex(x) {
             // Remove whitspace and hash sign
             x = x.replace(/[\s#;]*/g,'');
             // Handle 3-digit shorthand
@@ -230,21 +230,21 @@ var Color = (function(){
                 x = '0'+x;
             }
             return x;
-		},
-		
-		int: function parseFlatInt(n) {
+        },
+        
+        int: function parseFlatInt(n) {
             var i = parseInt(n);
             if (isNull(i)) return 0;
             return clamp(n, 0, BYTE3);
-		},
-		
-		rgb: function parseRGB(r,g,b) {
-			return { 
+        },
+        
+        rgb: function parseRGB(r,g,b) {
+            return { 
                 r: parse.byte(r), 
                 g: parse.byte(g), 
                 b: parse.byte(b)
-			};
-		},
+            };
+        },
         
         rgba: function parseRGBA(r, g, b, a) {
             return { 
@@ -252,7 +252,7 @@ var Color = (function(){
                 g: parse.byte(g), 
                 b: parse.byte(b),
                 a: parse.unit(a)
-			};
+            };
         },
         
         unit: function parseUnit(n) {
@@ -260,51 +260,51 @@ var Color = (function(){
             if (isNull(i)) return 1;
             return clamp(i, 0, 1);
         },
-		
-		byte: function parseByte(n) {
-			var i = parseInt(n);
-			if (isNull(i)) return 0;
-			return clamp(i, 0, BYTE);
-		}
-	}
-	
-	/*** Conversion ***/
-	var cnv = {
-		hex_int: function hexToInt(x){
-			var hex = parse.hex(x);
-			return parseInt(hex, 16);
-		},
-		
-		hex_rgb: function hexToRgb(x){
-			var int = cnv.hex_int(x);
-			return cnv.int_rgb(int);
-		},
-		
-		int_hex: function intToHex(n){
-			var int = parse.int(n);
-			return parse.hex(int.toString(16));
-		},
-		
-		int_rgb: function intToRgb(n){
-			var int = parse.int(n);
-			return {
-				r: (int & 0xff0000) >> 16,
-				g: (int & 0x00ff00) >> 8,
-				b:  int & 0x0000ff
-			}
-		},
-		
-		rgb_hex: function rgbToHex(r,g,b){
-			var int = cnv.rgb_int(r,g,b);
-			return cnv.int_hex(int);
-		},
-		
-		rgb_int: function rgbToInt(r,g,b){
-			return (parse.byte(r) << 16) + 
-					(parse.byte(g) << 8) + 
-					parse.byte(b);
-		}
-	};
+        
+        byte: function parseByte(n) {
+            var i = parseInt(n);
+            if (isNull(i)) return 0;
+            return clamp(i, 0, BYTE);
+        }
+    }
+    
+    /*** Conversion ***/
+    var cnv = {
+        hex_int: function hexToInt(x){
+            var hex = parse.hex(x);
+            return parseInt(hex, 16);
+        },
+        
+        hex_rgb: function hexToRgb(x){
+            var int = cnv.hex_int(x);
+            return cnv.int_rgb(int);
+        },
+        
+        int_hex: function intToHex(n){
+            var int = parse.int(n);
+            return parse.hex(int.toString(16));
+        },
+        
+        int_rgb: function intToRgb(n){
+            var int = parse.int(n);
+            return {
+                r: (int & 0xff0000) >> 16,
+                g: (int & 0x00ff00) >> 8,
+                b:  int & 0x0000ff
+            }
+        },
+        
+        rgb_hex: function rgbToHex(r,g,b){
+            var int = cnv.rgb_int(r,g,b);
+            return cnv.int_hex(int);
+        },
+        
+        rgb_int: function rgbToInt(r,g,b){
+            return (parse.byte(r) << 16) + 
+                    (parse.byte(g) << 8) + 
+                    parse.byte(b);
+        }
+    };
 
     function clamp(val, min, max) {
         if (val > max) val = max;
@@ -321,17 +321,17 @@ var Color = (function(){
         console.error(message);
     }
     
-	/*** Static Properties ***/
-	Color.util = util;
-	
+    /*** Static Properties ***/
+    Color.util = util;
+    
     Color.inv = util.not;
-	Color.add = util.add;
-	Color.avg = util.avg;
-	Color.sub = util.sub;
-	Color.and = util.and;
-	Color.or = util.or;
-	Color.xor = util.xor;
-	Color.random = util.random;
+    Color.add = util.add;
+    Color.avg = util.avg;
+    Color.sub = util.sub;
+    Color.and = util.and;
+    Color.or = util.or;
+    Color.xor = util.xor;
+    Color.random = util.random;
     
     Color.rgb = function(r,g,b) {
         return new Color(
@@ -348,19 +348,19 @@ var Color = (function(){
             parse.byte(a)
         );
     };
-	
-	Color.WHITE = 	new Color(0xffffff);
-	Color.RED = 	new Color(0xff0000);
-	Color.GREEN = 	new Color(0x00ff00);
-	Color.BLUE = 	new Color(0x0000ff);
-	Color.YELLOW = 	new Color(0xffff00);
-	Color.CYAN = 	new Color(0x00ffff);
-	Color.MAGENTA = new Color(0xff00ff);
-	Color.BLACK = 	new Color(0x0);
+    
+    Color.WHITE =   new Color(0xffffff);
+    Color.RED =     new Color(0xff0000);
+    Color.GREEN =   new Color(0x00ff00);
+    Color.BLUE =    new Color(0x0000ff);
+    Color.YELLOW =  new Color(0xffff00);
+    Color.CYAN =    new Color(0x00ffff);
+    Color.MAGENTA = new Color(0xff00ff);
+    Color.BLACK =   new Color(0x0);
     Color.TRANSPARENT = new Color(0,0,0,0);
-	
-	/** Return constructor **/
-	return Color;
+    
+    /** Return constructor **/
+    return Color;
 })();
 
 // #19CB97
---
+++ colortest.js
@@ -0,0 +1,85 @@
+function colortest(){
+    var c;
+    var passed = 0;
+    var failed = 0;
+    
+    function assert(actual, expected) {
+        if (typeof(actual)=='object') actual = JSON.stringify(actual);
+        if (typeof(expected)=='object') expected = JSON.stringify(expected);
+        if (actual === expected) {
+            passed++;
+            return true;
+        }
+        console.error('Actual: ' + actual + '\t Expected: ' + expected);
+        failed++;
+        return false;
+    }
+    
+    function assertError(funct, expectederror) {
+        var result = '';
+        try {
+            funct();
+        }
+        catch (e) {
+            result = e[0];
+        }
+        assert(result, expectederror);
+    }
+    
+    function rgb(r,g,b) {
+        return { r:r, g:g, b:b, a:1 };
+    };
+    
+    function rgba(r,g,b,a) {
+        return { r:r, g:g, b:b, a:a };
+    };
+    
+    // Valid hex constructors
+    assert(new Color('#0077ff'), rgb(0,119,255) );
+    assert(new Color('0077ff'), rgb(0,119,255) );
+    assert(new Color('#07f'), rgb(0,119,255) );
+    assert(new Color('07f'), rgb(0,119,255) );
+    
+    // Valid int constructors
+    assert(new Color(0x0077ff), rgb(0,119,255) );
+    assert(new Color(30719), rgb(0,119,255) );
+    assert(new Color(255), rgb(0,0,255) );
+    assert(new Color(0), rgb(0,0,0) );
+    assert(new Color(), rgb(0,0,0) );
+    
+    // Valid rgba constructors
+    assert(new Color(0, 119, 255), rgb(0,119,255) );
+    assert(new Color(0, 119, 255, 0.3), rgba(0,119,255,0.3) );
+    assert(new Color({r:0, g:119, b:255}), rgb(0,119,255) );
+    assert(new Color({r:0, g:119, b:255, a:0.3}), rgba(0,119,255,0.3) );
+    
+    // Invalid constructors
+    assertError(function(){ Color(0xffcc9977); }, 'InvalidArgumentsException');
+    assertError(function(){ Color(16, 32, 64, 128, 255); }, 'InvalidArgumentsException');
+    
+    var c0 = new Color(0x0077ff);
+    
+    assert(c0.hex(), '0077ff');
+    assert(c0.toString(), '#0077ff');
+    
+    c0.a = 0.4;
+    assert(c0.hex(), '0077ff');
+    assert(c0.toString(), 'rgba(0,119,255,0.4)');
+    
+    var c1 = new Color(255, 200, 64, 0.7);
+    
+    assert(c1.hex(), 'ffc840');
+    assert(c1.toString(), 'rgba(255,200,64,0.7)');
+    assert(c1.valueOf(), 0xffc840);
+    
+    assert(c0.not(), rgb(255, 136, 0) );
+    assert(c0.add(c1), rgb(255, 255, 255) );
+    assert(c0.sub(c1), rgb(0, 0, 191) );
+    assert(c0.and(c1), rgb(0, 64, 64) );
+    assert(c0.or(c1), rgb(255, 255, 255) );
+    assert(c0.xor(c1), rgb(255, 191, 191) );
+    
+    console.log('Passed: ' + passed);
+    console.log('Failed: ' + failed);
+}
+
